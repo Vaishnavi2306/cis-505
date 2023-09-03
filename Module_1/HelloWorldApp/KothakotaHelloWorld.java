dac6d484b2afdae5c2c089b635efb506da3785ec
Vaishnavi <vkothakota@my365.bellevue.edu>
2023-09-02 22:45:50 -0700

    Added a .gitignore file.

---
+++ Module_1/HelloWorldApp/KothakotaHelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWorldApp;
+
+public class KothakotaHelloWorld {
+
+	public static void main(String[] args) {
+		String firstName, lastName;
+		firstName="Vaishnavi";
+		lastName="Kothakota";
+		System.out.printf("Hello World from " +firstName+ " " +lastName);
+	}
+
+}
