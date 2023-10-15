97a395fa792c9c4d06d46fad706c038e5b3b58e5
Vaishnavi <vkothakota@my365.bellevue.edu>
2023-10-15 10:49:19 -0700

    Adds Future Value App

---
+++ Module_7/FutureValueApp/KothakotaFutureValueApp.java
@@ -0,0 +1,89 @@
+package FutureValueApp;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/*
+ * JavaFX application for Future Value app.
+ */
+public class KothakotaFutureValueApp extends Application {
+	
+	private TextField textField1 = new TextField();
+    private TextField textField2 = new TextField();
+    private TextArea textArea = new TextArea();
+    private Label label1 = new Label("Label 1");
+    private Label lblYears = new Label("Years:");
+    private Label lblInterestRateFormat = new Label("Enter 11.1% as 11.1");
+    private Label lblInterestRate = new Label("Interest Rate:");
+    private Label lblMonthlyPayment = new Label("Monthly Payment:");
+    private ComboBox<Integer> comboBox = new ComboBox<>();
+    private Button btnClear = new Button("Clear");
+    private Button btnCalculate = new Button("Calculate");
+
+	public static void main(String[] args) {
+        launch(args);
+    }
+	
+	@Override
+    public void start(Stage primaryStage) {
+		lblInterestRateFormat.setTextFill(Color.RED);
+		ObservableList<Integer> comboBoxValues = FXCollections.observableArrayList();
+        for (int i = 1; i <= 10; i++) {
+            comboBoxValues.add(i);
+        }
+        comboBox.setItems(comboBoxValues);
+        textField1.setPrefWidth(200); // Set your desired width
+        textField2.setPrefWidth(200); // Set your desired width
+        comboBox.setPrefWidth(200);
+        textArea.setPrefWidth(200);
+        
+	    // Create a GridPane
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setPadding(new javafx.geometry.Insets(11.5, 12.5, 13.5, 14.5));
+        gridPane.setHgap(5.5);
+        gridPane.setVgap(5.5);
+        
+        // Add components to the GridPane
+        gridPane.add(lblMonthlyPayment, 0, 0);
+        gridPane.add(textField1, 1, 0);
+        gridPane.add(lblInterestRate, 0, 1);
+        gridPane.add(textField2, 1, 1);
+        gridPane.add(lblInterestRateFormat, 1, 2);
+        gridPane.setHalignment(lblInterestRateFormat, HPos.RIGHT);
+        gridPane.add(lblYears, 0, 3);
+        gridPane.add(comboBox, 1, 3);
+        
+        HBox actionBtnContainer = new HBox();
+        actionBtnContainer.setPadding(new Insets(15, 0, 15, 30));
+        actionBtnContainer.setSpacing(10);
+        actionBtnContainer.getChildren().add(btnClear);
+        actionBtnContainer.getChildren().add(btnCalculate);
+        gridPane.add(actionBtnContainer, 1, 4);
+        gridPane.setHalignment(actionBtnContainer, HPos.RIGHT);
+        // Add TextArea with column span to cover 2 columns
+        gridPane.add(textArea, 0, 5, 2, 1);
+        
+        // Create a scene and set it on the stage
+        Scene scene = new Scene(gridPane);
+        primaryStage.setTitle("Kothakota Future Value App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	}
+	
+}
---
+++ Module_7/module-info.java
@@ -0,0 +1,6 @@
+module Module_7 {
+    requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.base;
+    exports FutureValueApp;
+}
